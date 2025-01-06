3fd62ddea8c24dd45d2a88b13f89a09f525b20a6
maxilong258 <adc3080027554@gmail.com>
2025-01-06 15:02:15 +0800

    feat: init

---
+++ config/site.ts
@@ -0,0 +1,15 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "tube_",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+  },
+}
---
+++ lib/fonts.ts
@@ -0,0 +1,21 @@
+import { JetBrains_Mono as FontMono, Inter as FontSans, Geist, Geist_Mono } from "next/font/google"
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export const fontMono = FontMono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
